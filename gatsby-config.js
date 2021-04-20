e8329b138a68d7f92c147bfc8945fcab2b3f66f9
Mickael Daniel <daniel.mickael@gmail.com>
2021-04-20 12:36:52 +0200

    Update for scope package

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,4 +1,4 @@
-const withDefault = require('@rocketseat/gatsby-theme-docs-core/util/with-default');
+const withDefault = require('@mklabs/gatsby-theme-docs-core/util/with-default');
 
 module.exports = (options) => {
   const themeOtions = withDefault(options);
@@ -18,7 +18,7 @@ module.exports = (options) => {
     },
     plugins: [
       {
-        resolve: `@rocketseat/gatsby-theme-docs-core`,
+        resolve: `@mklabs/gatsby-theme-docs-core`,
         options: themeOtions,
       },
       `gatsby-plugin-catch-links`,
